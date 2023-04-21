5daeeefbf751023eac3c7d2b95f0bd20195f0846
Levi Noecker <levi.noecker@gmail.com>
2023-04-21 06:46:10 -0500

    WIP: Struggling to use a HashMap to speed it up

--- 014-longest-collatz-sequence/src/main.rs
+++ 014-longest-collatz-sequence/src/main.rs
@@ -12,30 +12,98 @@
 
 // NOTE: Once the chain starts the terms are allowed to go above one million.
 
-fn next_seq(num: usize) -> usize {
+use std::collections::HashMap;
+// use std::ops::Add;
+
+#[derive(Debug)]
+struct Sequence {
+    num: usize,
+    chain_len: usize,
+    hash_map: HashMap<usize, usize>,
+}
+
+// impl Add for Point {
+//     type Output = Self;
+
+//     fn add(self, other: Self) -> Self {
+//         Self {
+//             x: self.x + other.x,
+//             y: self.y + other.y,
+//         }
+//     }
+// }
+
+fn next_seq(mut hash_map: HashMap<usize, usize>, num: usize) -> Sequence {
+    if hash_map.contains_key(&num) {
+        let chain_len = *hash_map.get(&num).unwrap();
+        return Sequence {
+            num,
+            chain_len,
+            hash_map,
+        };
+    }
+
     let next = match num % 2 {
         0 => num / 2,
         1 => num * 3 + 1,
         _ => unreachable!(),
     };
 
+    &mut hash_map.insert(num, next);
+
     match next {
-        1 => 2,
-        _ => 1 + next_seq(next),
+        1 => Sequence {
+            num: num,
+            chain_len: 2,
+            hash_map,
+        },
+        _ => {
+            let next_seq = next_seq(hash_map, next);
+            Sequence {
+                num: num,
+                chain_len: next_seq.chain_len + 1,
+                hash_map: next_seq.hash_map,
+            }
+        }
     }
 }
 
 #[test]
 fn test_next_seq() {
-    assert_eq!(next_seq(13), 10);
+    let mut hash_map: HashMap<usize, usize> = HashMap::new();
+    let next = next_seq(hash_map, 13);
+    assert_eq!(next.chain_len, 10);
+}
+
+fn longest_collatz_chain_under(limit: usize) -> Sequence {
+    let mut hash_map: HashMap<usize, usize> = HashMap::new();
+
+    let mut longest_seq: Sequence;
+
+    for num in limit..=1 {
+        let collatz_seq = next_seq(hash_map, num);
+        hash_map = collatz_seq.hash_map;
+
+        if &collatz_seq.chain_len > &longest_seq.chain_len {
+            println!("New longest {:?}", longest_seq);
+            longest_seq = collatz_seq;
+        }
+    }
+
+    longest_seq
 }
 
 fn main() {
+    let mut hash_map: HashMap<usize, usize> = HashMap::new();
+
     let mut longest_chain = (0, 0);
+
     for num in 1..1_000_000 {
-        let collatz_seq = next_seq(num);
-        if collatz_seq > longest_chain.1 {
-            longest_chain = (num, collatz_seq);
+        let collatz_seq = next_seq(hash_map, num);
+        hash_map = collatz_seq.hash_map;
+
+        if collatz_seq.chain_len > longest_chain.1 {
+            longest_chain = (collatz_seq.num, collatz_seq.chain_len);
             println!("New longest {:?}", longest_chain);
         }
     }
