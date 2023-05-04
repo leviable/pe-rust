528defb65420d6224da768df2350842d36db56db
Levi Noecker <levi.noecker@gmail.com>
2023-05-04 06:43:10 -0500

    Problem 15 complete
     - Switched from tracking all routes via a hashmap to only tracking the
       know routes of previous points, summed
     - I had done this previously, but didn't correctly identify how to
       calculate routes by summing the previously known routes

--- 015-lattice-paths/src/main.rs
+++ 015-lattice-paths/src/main.rs
@@ -2,11 +2,12 @@
 
 // How many such routes are there through a 20×20 grid?
 use std::collections::HashMap;
+use std::time::Instant;
 
 struct Grid {
     origin: Point,
     end: Point,
-    routes: HashMap<Point, Vec<Route>>,
+    routes: HashMap<Point, usize>,
 }
 
 impl Grid {
@@ -53,66 +54,26 @@ impl PartialEq for Point {
     }
 }
 
-#[derive(Clone)]
-struct Route {
-    points: Vec<Point>,
-}
-
-impl Route {
-    fn new(point: Point) -> Self {
-        Self {
-            points: vec![point],
-        }
-    }
-
-    fn add(&mut self, point: Point) {
-        self.points.push(point);
-    }
-}
-
-fn find_routes_at_point(grid: &mut Grid, point: &Point) -> Vec<Route> {
-    println!("00000000000000000000000000000000000000");
+fn find_routes_at_point(grid: &mut Grid, point: &Point) -> usize {
     if *point == grid.origin {
-        return vec![Route::new(point.clone())];
+        return 1;
     }
 
     if !grid.routes.contains_key(point) {
-        println!("11111111111111111111111111111111111111");
-        println!("{:?}", point);
-        let mut found_routes: Vec<Route> = vec![];
+        let mut found_routes: usize = 0;
         if grid.point_above(point) {
-            println!("33333333333333333333333333333333333333");
             let p = grid.get_point_above(point);
-            found_routes.extend(find_routes_at_point(grid, &p));
-            println!("44444444444444444444444444444444444444");
+            found_routes += find_routes_at_point(grid, &p);
         }
         if grid.point_left(point) {
-            println!("55555555555555555555555555555555555555");
             let p = grid.get_point_left(point);
-            found_routes.extend(find_routes_at_point(grid, &p));
-            println!("66666666666666666666666666666666666666");
+            found_routes += find_routes_at_point(grid, &p);
         }
 
-        println!(
-            "77777777777777777777777777777777777777 - {}",
-            found_routes.len()
-        );
-        for idx in 0..found_routes.len() {
-            found_routes[idx].add(point.clone());
-            // println!("88888888888888888888888888888888888888");
-        }
-        println!("88888888888888888888888888888888888888");
-
-        grid.routes.insert(point.clone(), found_routes.clone());
-    } else {
-        println!("22222222222222222222222222222222222222");
-        println!("{:?}", point);
+        grid.routes.insert(point.clone(), found_routes);
     }
 
-    println!("99999999999999999999999999999999999999");
-    let my_routes: Vec<Route> = grid.routes.get(point).unwrap().clone();
-    println!("Point {:?} - Route Count = {}", point, my_routes.len());
-    println!("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    let my_routes = grid.routes.get(point).unwrap().clone();
 
     my_routes
 }
@@ -122,12 +83,14 @@ fn test_find_routes_at_point() {
     let mut test_grid = Grid::new(2);
     let starting_point = test_grid.end.clone();
     let routes = find_routes_at_point(&mut test_grid, &starting_point);
-    assert_eq!(routes.len(), 6);
+    assert_eq!(routes, 6);
 }
 
 fn main() {
-    let mut grid = Grid::new(3);
+    let start = Instant::now();
+    let mut grid = Grid::new(20);
     let starting_point = grid.end.clone();
     let routes = find_routes_at_point(&mut grid, &starting_point);
-    println!("Total routes for 20: {}", routes.len());
+    println!("Total routes for 20: {}", routes);
+    println!("Took {}s to complete", start.elapsed().as_secs_f64());
 }
