a29e6dbb245e50b2b62898840f51a404a933fe30
Levi Noecker <levi.noecker@gmail.com>
2023-06-29 12:35:02 -0500

    Problem 050 complete
    - Debug in 2.30 s
     - Release in 256 ms

--- 050-consecutive-prime-sum/src/main.rs
+++ 050-consecutive-prime-sum/src/main.rs
@@ -12,20 +12,30 @@
 use primes::{is_prime, PrimeSet, Sieve};
 use std::time::Instant;
 
-fn longest_chain(longest_known: usize, start: usize, limit: usize) -> (usize, usize) {
+fn longest_chain(
+    longest_known_chain_count: usize,
+    chain_start: usize,
+    prime_limit: usize,
+) -> (usize, usize) {
     let mut longest: usize = 0;
     let mut chain_prime: usize = 0;
     let mut sum: usize = 0;
     let mut pset = Sieve::new();
-    for (idx, prime) in pset.iter().enumerate().skip(start) {
+    for (idx, prime) in pset.iter().enumerate().skip(chain_start) {
+        // Even though we call ".skip()", the idx will be as if we hadn't skipped any.
+        // Need to make sure to subtract chain_start from idx for actual chain size
         sum += prime as usize;
-        let foo: i64 = longest_known as i64 + 1 + start as i64 - idx as i64;
-        if sum >= limit || prime as i64 * foo >= limit as i64 {
-            return (chain_prime.try_into().unwrap(), longest);
+        // If the minimum number of remaining chain slots * the next prime in the chain
+        // is greater than our prime limit, then there is no point in doing further
+        // calculations; break out
+        let minimum_needed: i64 =
+            longest_known_chain_count as i64 + 1 + chain_start as i64 - idx as i64;
+        if sum >= prime_limit || prime as i64 * minimum_needed >= prime_limit as i64 {
+            return (chain_prime, longest);
         }
 
-        if is_prime(sum.try_into().unwrap()) && idx - start + 1 > longest {
-            longest = idx - start + 1;
+        if is_prime(sum.try_into().unwrap()) && idx - chain_start + 1 > longest {
+            longest = idx - chain_start + 1;
             chain_prime = sum;
         }
     }
@@ -33,33 +43,33 @@ fn longest_chain(longest_known: usize, start: usize, limit: usize) -> (usize, us
     (chain_prime, longest)
 }
 
-fn find_longest(limit: usize) -> (usize, usize) {
-    let mut longest: usize = 0;
-    let mut longest_prime: usize = 0;
+fn find_longest_chain(prime_limit: usize) -> (usize, usize) {
+    let mut longest_chain_count: usize = 0;
+    let mut longest_chain_prime: usize = 0;
     for start in 0.. {
-        if start * longest >= limit {
+        if start * longest_chain_count >= prime_limit {
             break;
         }
-        let (prime, found) = longest_chain(longest, start, limit);
+        let (chain_prime, chain_count) = longest_chain(longest_chain_count, start, prime_limit);
 
-        if found > longest {
-            longest = found;
-            longest_prime = prime;
+        if chain_count > longest_chain_count {
+            longest_chain_count = chain_count;
+            longest_chain_prime = chain_prime;
         }
     }
 
-    (longest, longest_prime)
+    (longest_chain_count, longest_chain_prime)
 }
 
 #[test]
 fn test_find_largest_under() {
-    assert_eq!(find_longest(100), (6, 41));
-    assert_eq!(find_longest(1000), (21, 953));
+    assert_eq!(find_longest_chain(100), (6, 41));
+    assert_eq!(find_longest_chain(1000), (21, 953));
 }
 
 fn main() {
     let start = Instant::now();
-    let solution = find_longest(1_000_000);
+    let solution = find_longest_chain(1_000_000);
     println!("Solution: {:?}", solution);
     println!("Time elapsed: {}", start.elapsed().as_secs_f64());
 }
