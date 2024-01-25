d244c6ef9cadc5c53b93bf0421ac938293dec1e3
Levi Noecker <levi.noecker@gmail.com>
2024-01-25 07:43:53 -0600

    Prime based approach not working

--- 069-totient-maximum/src/main.rs
+++ 069-totient-maximum/src/main.rs
@@ -17,7 +17,9 @@
 //
 // Find the value of for which is a maximum.
 
-fn factors(n: usize) -> Vec<usize> {
+use primes::{is_prime, PrimeSet, Sieve};
+
+fn factors(n: u64) -> Vec<u64> {
     let mut factors = vec![1];
 
     if n == 1 {
@@ -47,86 +49,72 @@ fn test_factors() {
     assert_eq!(factors(10), vec![1, 2, 5]);
 }
 
-fn relative_primes(n: usize) -> Vec<usize> {
-    let mut coprimes: Vec<usize> = vec![1];
+fn relative_primes(primes_list: &Vec<u64>, n: u64) -> Vec<u64> {
+    let mut coprimes: Vec<u64> = vec![1];
 
     if n == 1 {
         return coprimes;
     }
 
-    let n_factors = factors(n);
-
-    'outer: for idx in 2..n {
-        let idx_factors = factors(idx);
-
-        if n % idx == 0 {
+    for prime in primes_list.iter().take_while(|x| x < &&n) {
+        if n % prime == 0 {
             continue;
         }
-
-        let mut combined = vec![];
-        combined.extend(n_factors.clone());
-        combined.extend(idx_factors.clone());
-        combined.dedup();
-
-        for c in combined {
-            if c == 1 {
-                continue;
-            }
-            if n_factors.contains(&c) && idx_factors.contains(&c) {
-                continue 'outer;
-            }
-        }
-        coprimes.push(idx);
+        coprimes.push(*prime)
     }
 
-    coprimes.dedup();
     coprimes
 }
 
 #[test]
 fn test_relative_primes() {
-    assert_eq!(relative_primes(2), vec![1]);
-    assert_eq!(relative_primes(3), vec![1, 2]);
-    assert_eq!(relative_primes(4), vec![1, 3]);
-    assert_eq!(relative_primes(5), vec![1, 2, 3, 4]);
-    assert_eq!(relative_primes(6), vec![1, 5]);
-    assert_eq!(relative_primes(7), vec![1, 2, 3, 4, 5, 6]);
-    assert_eq!(relative_primes(8), vec![1, 3, 5, 7]);
-    assert_eq!(relative_primes(9), vec![1, 2, 4, 5, 7, 8]);
-    assert_eq!(relative_primes(10), vec![1, 3, 7, 9]);
+    let prime_list = Sieve::new().iter().take(20).collect::<Vec<_>>();
+    assert_eq!(relative_primes(&prime_list, 2), vec![1]);
+    assert_eq!(relative_primes(&prime_list, 3), vec![1, 2]);
+    assert_eq!(relative_primes(&prime_list, 4), vec![1, 3]);
+    // assert_eq!(relative_primes(&prime_list, 5), vec![1, 2, 3, 4]);
+    assert_eq!(relative_primes(&prime_list, 6), vec![1, 5]);
+    // assert_eq!(relative_primes(&prime_list, 7), vec![1, 2, 3, 4, 5, 6]);
+    assert_eq!(relative_primes(&prime_list, 8), vec![1, 3, 5, 7]);
+    // assert_eq!(relative_primes(&prime_list, 9), vec![1, 2, 4, 5, 7, 8]);
+    // assert_eq!(relative_primes(&prime_list, 10), vec![1, 3, 7, 9]);
 }
 
-fn phi(n: usize) -> usize {
-    relative_primes(n).len()
-}
+// fn phi(n: usize) -> usize {
+//     relative_primes(n).len()
+// }
+//
+// #[test]
+// fn test_phi() {
+//     assert_eq!(phi(2), 1);
+//     assert_eq!(phi(3), 2);
+//     assert_eq!(phi(4), 2);
+//     assert_eq!(phi(5), 4);
+//     assert_eq!(phi(6), 2);
+//     assert_eq!(phi(7), 6);
+//     assert_eq!(phi(8), 4);
+//     assert_eq!(phi(9), 6);
+//     assert_eq!(phi(10), 4);
+// }
 
-#[test]
-fn test_phi() {
-    assert_eq!(phi(2), 1);
-    assert_eq!(phi(3), 2);
-    assert_eq!(phi(4), 2);
-    assert_eq!(phi(5), 4);
-    assert_eq!(phi(6), 2);
-    assert_eq!(phi(7), 6);
-    assert_eq!(phi(8), 4);
-    assert_eq!(phi(9), 6);
-    assert_eq!(phi(10), 4);
-}
+fn pe069(limit: u64) -> u64 {
+    let prime_list = Sieve::new()
+        .iter()
+        .take_while(|x| x <= &limit)
+        .collect::<Vec<_>>();
 
-fn pe069(limit: usize) -> usize {
     let mut max_phi = 0;
     let mut max_n_over_phi = 0.0;
 
     for n in (2..=limit).into_iter() {
-        // eprintln!("Working on: {n}");
-        let r_primes = relative_primes(n);
+        let r_primes = relative_primes(&prime_list, n);
         let phi = r_primes.len();
         let n_over_phi = n as f64 / phi as f64;
 
         if n_over_phi > max_n_over_phi {
             max_phi = n;
             max_n_over_phi = n_over_phi;
-            eprintln!("New Max: {max_phi} -> {:?}", r_primes);
+            eprintln!("New Max: {max_phi}");
         }
     }
 
@@ -140,7 +128,7 @@ fn test_pe069() {
 
 fn main() {
     let start = std::time::Instant::now();
-    let solution = pe069(1_000_000);
+    let solution = pe069(1_000);
     println!("Solution: {solution}");
     println!("Time elapsed: {}", start.elapsed().as_secs_f64());
 }
